ea74c407980d93e614497ecb0713c83731d36858
savanna chiang <savannaac@gmail.com>
2021-05-18 23:58:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+document.getElementById("main").remove()
+
+let newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "Savanna is the champion"
+
+// function victoryHeader() {
+//     let newHeader = document.createElement("h1");
+//     let newHeaderVictory = document.createTextNode("victory");
+//     newHeader.appendChild(newHeaderVictory)
+//     document.body.appendChild(newHeader)  
+// }
+
+
+
+
+
